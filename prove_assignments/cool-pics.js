3adb88c98cad72590ecf8342370046ef06eada81
Lmacconnell2 <mac23068@byui.edu>
2025-01-25 23:29:51 -0700

    oh yes js

--- prove_assignments/cool-pics.js
+++ prove_assignments/cool-pics.js
@@ -1,4 +1,8 @@
 const menuShow = document.querySelector(".button");
+const gallery = document.querySelector(".images");
+const body = document.querySelector("body");
+
+
 
 function showMenu() {
     
@@ -15,7 +19,32 @@ function handleResize() {
     }  
 }
 
+function openViewerThing(event) {
+    const clickedElement = event.target;
+    let image = event.target.src;
+    let alt = event.target.alt;
+    let pic = image.replace("-sm.jpeg", "-full.jpeg");
+    openViewer(pic, alt);
+}
+
+function openViewer(pic, alt) {
+    body.insertAdjacentHTML("afterbegin", `<div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}">
+    </div>`);
+    const x = document.querySelector(".close-viewer");
+    x.addEventListener("click", closeViewer);
+}
+
+function closeViewer() {
+    const removeViewer = document.querySelector(".viewer");
+    removeViewer.remove()
+}
+
 
 menuShow.addEventListener('click', showMenu);
 handleResize();
 window.addEventListener("resize", handleResize);
+gallery.addEventListener("click", openViewerThing);
+
+
